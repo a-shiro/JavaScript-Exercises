f0845f2da57132289e8f8f32b150376e7adb7d94
a-shiro <alekmitov20@gmail.com>
2022-08-30 18:05:19 +0300

    Added Exercise

---
+++ JS Basics/3. Conditional Statements Advanced/Lab/09. Fruit or Vegetable.js
@@ -0,0 +1,29 @@
+function checkType(input) {
+    let product = input[0];
+
+    switch (product) {
+        case 'banana':
+        case 'apple':
+        case 'kiwi':
+        case 'cherry':
+        case 'lemon':
+        case 'grapes':
+            console.log('fruit');
+            break;
+
+        case 'tomato':
+        case 'cucumber':
+        case 'pepper':
+        case 'carrot':
+            console.log('vegetable');
+            break;
+
+        default:
+            console.log('unknown');
+    }
+}
+
+checkType(['banana']);
+checkType(['apple']);
+checkType(['tomato']);
+checkType(['water']);
