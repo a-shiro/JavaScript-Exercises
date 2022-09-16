6c57abd8e794d405c91c2cc799cfdcacc8f6f8e5
a-shiro <alekmitov20@gmail.com>
2022-09-16 11:41:55 +0300

    Added Exercise

---
+++ JS Fundamentals/5. Array Advanced/Exercise/02. Distinct Array.js
@@ -0,0 +1,14 @@
+function solve(array) {
+    let uniqueNumbers = [];
+
+    array.forEach(n => {
+        if (!uniqueNumbers.includes(n)) {
+            uniqueNumbers.push(n);
+        }
+    })
+
+    console.log(...uniqueNumbers);
+}
+
+solve([7, 8, 9, 7, 2, 3, 4, 1, 2]);
+solve([20, 8, 12, 13, 4, 4, 8, 5]);
