0d4bcfc204264609457e3158bc040035bcf50620
a-shiro <alekmitov20@gmail.com>
2022-09-27 18:25:48 +0300

    Added Exercise

---
+++ JS Advanced/3. Objects and Compositions/Exercise/9. Heroes.js
@@ -0,0 +1,44 @@
+function createHero() {
+    return {
+        mage: function (name) {
+            return {
+                name,
+                'health': 100,
+                'mana': 100,
+                cast: function (spell) {
+                    console.log(`${this.name} cast ${spell}`);
+                    this.mana--;
+                }
+            }
+        },
+        fighter: function (name) {
+            return {
+                name,
+                'health': 100,
+                'stamina': 100,
+                fight: function () {
+                    console.log(`${this.name} slashes at the foe!`);
+                    this.stamina--;
+                }
+            }
+        }
+    };
+}
+
+let create = createHero();
+
+const scorcher = create.mage("Scorcher");
+
+scorcher.cast("fireball");
+
+scorcher.cast("thunder");
+
+scorcher.cast("light");
+
+const scorcher2 = create.fighter("Scorcher 2");
+
+scorcher2.fight();
+
+console.log(scorcher2.stamina);
+
+console.log(scorcher.mana);
