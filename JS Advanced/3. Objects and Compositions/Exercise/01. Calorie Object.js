4e9d5b7a98d94158db0a41aee181a25684622121
a-shiro <alekmitov20@gmail.com>
2022-09-27 15:12:47 +0300

    Added Exercise

---
+++ JS Advanced/3. Objects and Compositions/Exercise/01. Calorie Object.js
@@ -0,0 +1,11 @@
+function solve(array) {
+    let result = {};
+
+    for (let i = 0; i < array.length; i += 2) {
+       result[array[i]] = Number(array[i + 1]);
+    }
+
+    console.log(result);
+}
+
+solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
