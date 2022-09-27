55559fd78fc7c34b4c8fc79e8ceace841f81ac19
a-shiro <alekmitov20@gmail.com>
2022-09-27 17:30:59 +0300

    Added Exercise

---
+++ JS Advanced/3. Objects and Compositions/Exercise/06. Store Catalogue.js
@@ -0,0 +1,32 @@
+function solve(array) {
+    let catalogue = [];
+
+    for (let pair of array) {
+        let [product, price] = pair.split(' : ')
+
+        let underLetter = product[0];
+
+        if (!Object.keys(catalogue).includes(underLetter)) {
+            catalogue[underLetter] = [];
+        }
+
+        let duplicate = catalogue[underLetter].find(object => object.product === product);
+
+        if (!duplicate) {
+            catalogue[underLetter].push({
+                product,
+                'price': Number(price)
+            });
+        }
+    }
+
+    for (let letter of Object.keys(catalogue).sort((a, b) => a.localeCompare(b))) {
+        console.log(letter);
+
+        for (let item of catalogue[letter].sort((a, b) => a.product.localeCompare(b.product))) {
+            console.log(` ${item.product}: ${item.price}`);
+        }
+    }
+}
+
+solve(['Appricot : 20.4', 'Fridge : 1500', 'TV : 1499', 'Deodorant : 10', 'Boiler : 300', 'Apple : 1.25', 'Anti-Bug Spray : 15', 'T-Shirt : 10']);
