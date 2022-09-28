ea067847931a054a1b1f8b216951c2b9eb809967
a-shiro <alekmitov20@gmail.com>
2022-09-28 10:11:45 +0300

    Added Exercise

--- JS Advanced/4. DOM Introduction/Lab/07. Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced/4. DOM Introduction/Lab/07. Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,13 @@
 function extract(content) {
+    let text = document.getElementById('content').textContent;
 
+    let matches = text.match(/\(([^)]+)\)/g);
+
+    let result = [];
+
+    matches.forEach(function printing(element) {
+        result.push(element.slice(1, element.length - 1));
+    })
+
+    return result.join('; ');
 }
