091bbb748e457858ca3b2dfe376625023e8849ec
a-shiro <alekmitov20@gmail.com>
2022-09-28 09:06:01 +0300

    Added Exercise

--- JS Advanced/4. DOM Introduction/Lab/02. Collect List Items/CollectListItems.js
+++ JS Advanced/4. DOM Introduction/Lab/02. Collect List Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    let items = document.getElementsByTagName('li');
+    let textArea = document.getElementById('result');
+
+    for (let item of items) {
+        textArea.textContent += `${item.textContent}\n`
+    }
 }
--- JS Advanced/4. DOM Introduction/Lab/03. Sum Numbers/SumNumbers.js
+++ JS Advanced/4. DOM Introduction/Lab/03. Sum Numbers/SumNumbers.js
@@ -1,3 +1,6 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let n1 = Number(document.getElementById('num1').value);
+    let n2 = Number(document.getElementById('num2').value);
+
+    document.getElementById('sum').value = n1 + n2;
 }
