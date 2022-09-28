4bbd600f54fb0a7077fd1b64eabe52d99329ee79
a-shiro <alekmitov20@gmail.com>
2022-09-28 09:40:22 +0300

    Added Exercise

--- JS Advanced/4. DOM Introduction/Lab/06. Sum Table/SumTable.js
+++ JS Advanced/4. DOM Introduction/Lab/06. Sum Table/SumTable.js
@@ -1,3 +1,12 @@
 function sumTable() {
+    let dataCells = document.querySelectorAll('table tr');
 
+    let result = 0;
+
+    for (let i = 1; i < dataCells.length; i++) {
+        let children = dataCells[i].children;
+        result += Number(children[1].textContent);
+    }
+
+    document.getElementById('sum').textContent = String(result);
 }
