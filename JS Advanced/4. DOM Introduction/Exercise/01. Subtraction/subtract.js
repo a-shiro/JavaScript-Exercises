d73088cb9fbf79495f444f881890a50534f1f36c
a-shiro <alekmitov20@gmail.com>
2022-09-29 08:26:07 +0300

    Added Exercise

--- JS Advanced/4. DOM Introduction/Exercise/01. Subtraction/subtract.js
+++ JS Advanced/4. DOM Introduction/Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+    let n1 = Number(document.getElementById('firstNumber').value);
+    let n2 = Number(document.getElementById('secondNumber').value);
+
+    return document.getElementById('result').textContent = n1 - n2;
 }
