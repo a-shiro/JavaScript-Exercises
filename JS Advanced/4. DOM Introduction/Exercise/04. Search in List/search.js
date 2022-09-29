abddbfc4075095f78fbe4a61097eac13a9528302
a-shiro <alekmitov20@gmail.com>
2022-09-29 17:18:23 +0300

    Added Exercise

--- JS Advanced/4. DOM Introduction/Exercise/04. Search in List/search.js
+++ JS Advanced/4. DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   let towns = Array.from(document.getElementById('towns').children);
+    let searchedText = document.getElementById('searchText').value;
+    let matchesFound = 0;
+
+   for (let town of towns) {
+       if (town.textContent.includes(searchedText) && searchedText !== '') {
+           town.style.fontWeight = 'bold';
+           town.style.textDecoration = 'underline';
+           matchesFound++;
+       } else {
+           town.style.fontWeight = '';
+           town.style.textDecoration = 'none';
+       }
+   }
+   
+   document.getElementById('result').textContent = `${matchesFound} matches found`;
 }
+
