0b6f6054bed7b5302d318125a7f785323f6ba3b5
a-shiro <alekmitov20@gmail.com>
2022-09-29 16:50:46 +0300

    Added Exercise

--- JS Advanced/4. DOM Introduction/Exercise/03. Accordion/accordion.js
+++ JS Advanced/4. DOM Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let moreText = document.getElementById('extra');
+
+    if (moreText.style.display !== 'none') {
+        moreText.style.display = 'none';
+        document.getElementsByClassName('button')[0].textContent = 'More';
+
+    } else {
+        moreText.style.display = 'block';
+        document.getElementsByClassName('button')[0].textContent = 'Less';
+    }
 }
