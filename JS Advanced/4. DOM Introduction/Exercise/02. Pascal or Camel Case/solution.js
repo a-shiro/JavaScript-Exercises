6934c759ce9af9c3a565b642cc5e2a5c8b037997
a-shiro <alekmitov20@gmail.com>
2022-09-29 09:09:35 +0300

    Added Exercise

--- JS Advanced/4. DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ JS Advanced/4. DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO...
+    function convert(textInput, namingConvention) {
+        let result = [];
+        let iterable = textInput.split(' ')
+
+        switch (namingConvention) {
+            case 'Camel Case':
+                result.push(iterable.shift().toLowerCase());
+
+                for (let element of iterable) {
+                    element = element.toLowerCase();
+                    element = element[0].toUpperCase() + element.slice(1);
+
+                    result.push(element);
+                }
+                break;
+            case 'Pascal Case':
+                for (let element of iterable) {
+                    element = element.toLowerCase();
+                    element = element[0].toUpperCase() + element.slice(1);
+
+                    result.push(element);
+                }
+                break;
+            default:
+                result = ['Error!'];
+        }
+
+        return result.join('');
+    }
+
+    let textInput = document.getElementById('text').value;
+    let namingConvention = document.getElementById('naming-convention').value;
+
+    document.getElementById('result').textContent = convert(textInput, namingConvention);
 }
