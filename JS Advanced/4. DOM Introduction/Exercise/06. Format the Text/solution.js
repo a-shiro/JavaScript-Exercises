2e816ccaf2c374c8600dd8af94a0a86d216047fe
a-shiro <alekmitov20@gmail.com>
2022-09-30 17:21:58 +0300

    Added Exercise

--- JS Advanced/4. DOM Introduction/Exercise/06. Format the Text/solution.js
+++ JS Advanced/4. DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+    let text = document.getElementById('input').value.split('.');
+    text.pop();
+
+    if (text) {
+        let counter = 3;
+
+        for (let sentence of text) {
+            if (counter === 3) {
+                let p = document.createElement('p');
+                document.getElementById('output').appendChild(p);
+
+                counter = 0;
+            }
+
+            document.getElementById('output').lastChild.textContent += sentence + '.'
+            counter++;
+        }
+    }
 }
+
