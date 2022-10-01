8f50771d337b366587fa778049c875c26512e716
a-shiro <alekmitov20@gmail.com>
2022-10-01 16:37:55 +0300

    Added Exercise

--- JS Advanced/5. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
+++ JS Advanced/5. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
@@ -3,11 +3,11 @@ function addItem() {
     let userInput = document.getElementById('newItemText').value;
 
     ul.addEventListener('click', function (event) {
-        event.target.parentElement.remove()
+        event.target.parentElement.remove();
     });
 
     let li = document.createElement('li');
-    li.innerHTML = `${userInput} <a href="#">[Delete]</a>`
+    li.innerHTML = `${userInput} <a href="#">[Delete]</a>`;
 
     ul.appendChild(li);
 }
