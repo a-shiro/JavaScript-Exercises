1c47b7ded4fdde6a4a7164600fda2f11c4e6f882
a-shiro <alekmitov20@gmail.com>
2022-10-01 12:00:26 +0300

    Added Exercise

--- JS Advanced/5. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
+++ JS Advanced/5. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,20 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let tableContent = Array.from(document.querySelectorAll('tbody tr'));
+    let userInput = document.getElementsByName('email')[0].value;
+    let matchFound = false;
+
+    for (let tr of tableContent) {
+        let email = tr.children[1].textContent;
+
+        if (userInput === email) {
+            tr.remove();
+            document.getElementById('result').textContent = 'Deleted';
+            matchFound = true;
+            break;
+        }
+    }
+
+    if (!matchFound) {
+        document.getElementById('result').textContent = 'Not found.';
+    }
 }
