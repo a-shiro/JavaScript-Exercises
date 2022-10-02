76c61cc55c41bfc9d67cdba025f93f930987d469
a-shiro <alekmitov20@gmail.com>
2022-10-02 10:23:37 +0300

    Added Exercise

--- JS Advanced/5. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
+++ JS Advanced/5. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,13 @@
 function focused() {
-    console.log('TODO:...');
+    let nodes = Array.from(document.querySelectorAll('body div div input'));
+
+    for (let node of nodes) {
+        node.addEventListener('focus', function () {
+            node.parentElement.classList.add('focused');
+        })
+
+        node.addEventListener('blur', function () {
+            node.parentElement.classList.remove('focused');
+        })
+    }
 }
