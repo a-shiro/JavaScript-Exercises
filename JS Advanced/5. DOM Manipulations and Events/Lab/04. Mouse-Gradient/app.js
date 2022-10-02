6341caf3377ee8a34bdc7e09aae8ed1a33cbc621
a-shiro <alekmitov20@gmail.com>
2022-10-02 09:56:46 +0300

    Added Exercise

--- JS Advanced/5. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
+++ JS Advanced/5. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,14 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.getElementById('gradient');
+
+    gradient.addEventListener('mousemove', function (event) {
+        let power = event.offsetX / (event.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+
+        document.getElementById('result').textContent = power + '%'
+    });
+
+    gradient.addEventListener('mouseout', function (event) {
+        document.getElementById('result').textContent = "";
+    })
 }
