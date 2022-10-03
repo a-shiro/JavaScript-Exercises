bda2dc81942fe8f0452361745725c2583a8758b8
a-shiro <alekmitov20@gmail.com>
2022-10-03 11:21:58 +0300

    Added Exercise

--- JS Advanced/5. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ JS Advanced/5. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,18 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.getElementsByTagName('button'));
+
+    buttons.forEach(button => button.addEventListener('click', displayMore));
+
+    function displayMore(event) {
+        let profileLock = event.target.parentElement.querySelector('input[value=lock]');
+        let hiddenDiv = event.target.parentElement.querySelector('div');
+
+        if (!profileLock.checked && (hiddenDiv.style.display === '' || hiddenDiv.style.display === 'none')) {
+            hiddenDiv.style.display = 'block';
+            event.target.textContent = 'Hide it';
+        } else if (!profileLock.checked) {
+            hiddenDiv.style.display = 'none';
+            event.target.textContent = 'Show more';
+        }
+    }
 }
