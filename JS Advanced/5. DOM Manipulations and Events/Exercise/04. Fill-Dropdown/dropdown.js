1498f5c0b35fbc725bc60bb33cc4062ce6ebfd37
a-shiro <alekmitov20@gmail.com>
2022-10-03 11:29:00 +0300

    Added Exercise

--- JS Advanced/5. DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/5. DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let userText = document.getElementById('newItemText').value;
+    let userValue = document.getElementById('newItemValue').value;
+
+    let option = document.createElement('option');
+
+    option.textContent = userText;
+    option.value = userValue;
+
+    document.getElementById('menu').appendChild(option);
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
 }
