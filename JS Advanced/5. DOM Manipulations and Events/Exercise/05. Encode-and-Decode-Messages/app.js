00b078d516ca2ee12da6611e1ed75abad2104438
a-shiro <alekmitov20@gmail.com>
2022-10-03 13:37:19 +0300

    Added Exercise

--- JS Advanced/5. DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/5. DOM Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,29 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = Array.from(document.getElementsByTagName('button'));
+
+    buttons[0].addEventListener('click', encode);
+    buttons[1].addEventListener('click', decode);
+
+    function encode() {
+        let text = buttons[0].parentElement.querySelector('textarea').value.split('');
+
+
+        for (let [idx, element] of text.entries()) {
+            text[idx] = String.fromCharCode(element.charCodeAt(0) + 1);
+        }
+
+        buttons[1].parentElement.querySelector('textarea').value = text.join('');
+        buttons[0].parentElement.querySelector('textarea').value = '';
+    }
+
+    function decode() {
+        let text = buttons[1].parentElement.querySelector('textarea').value.split('');
+
+
+        for (let [idx, element] of text.entries()) {
+            text[idx] = String.fromCharCode(element.charCodeAt(0) - 1);
+        }
+
+        buttons[1].parentElement.querySelector('textarea').value = text.join('');
+    }
 }
