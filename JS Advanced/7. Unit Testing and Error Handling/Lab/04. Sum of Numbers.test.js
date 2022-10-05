128d3da7c4026c61b0c25309f7764edb4469ac07
a-shiro <alekmitov20@gmail.com>
2022-10-05 14:57:18 +0300

    Added Exercise

---
+++ JS Advanced/7. Unit Testing and Error Handling/Lab/04. Sum of Numbers.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const {sum} = require("./04. Sum of Numbers");
+
+describe('sum', () => {
+    it('function returns correct result with array of numbers', () => {
+        // Arrange
+        let array = [1, 2, 3];
+
+        //Act
+        let result = sum(array);
+
+        //Assert
+        expect(result).to.be.equal(6);
+    })
+})
