094d66bedd50a1fd4ab56d4a361eec2fb406b4bb
a-shiro <alekmitov20@gmail.com>
2022-10-07 10:29:50 +0300

    Added Exercise

--- JS Advanced/7. Unit Testing and Error Handling/Exercise/05. Notification/app.js
+++ JS Advanced/7. Unit Testing and Error Handling/Exercise/05. Notification/app.js
@@ -1,4 +1,11 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+    let hiddenDiv = document.getElementById('notification');
+    hiddenDiv.addEventListener('click', toggleVisibility);
+
+    hiddenDiv.textContent = message;
+    hiddenDiv.style.display = 'block';
+
+    function toggleVisibility() {
+        hiddenDiv.style.display = 'none';
+    }
 }
