42ab666f8b359d735513be2bb1ee6df1bfef84b9
a-shiro <alekmitov20@gmail.com>
2022-10-31 10:39:04 +0200

    Added Exercise

--- JS Advanced/9. Prototypes and Inheritance/Exercise/01. Array Extension.js
+++ JS Advanced/9. Prototypes and Inheritance/Exercise/01. Array Extension.js
@@ -1,6 +1,47 @@
-class ArrayExtended {
-    last (array) {
+let myArray = [1, 2, 3];
 
+Array.prototype.last = function () {
+    return this[this.length - 1];
+}
+
+// console.log(myArray.last());
+
+Array.prototype.skip = function (idx) {
+    if (typeof idx !== 'number') {
+        throw new Error('Given parameter is not of type Int!');
     }
 
+    if (idx > this.length || idx < 0) {
+        throw new Error('Index out of range');
+    }
+
+    return [...this].splice(idx);
 }
+
+// console.log(myArray.skip(1));
+
+Array.prototype.take = function (idx) {
+    if (typeof idx !== 'number') {
+        throw new Error('Given parameter is not of type Int!');
+    }
+
+    if (idx > this.length || idx < 0) {
+        throw new Error('Index out of range');
+    }
+
+    return [...this].splice(0, idx);
+}
+
+// console.log(myArray.take(2));
+
+Array.prototype.sum = function () {
+    return this.reduce((total, n) => total + n, 0);
+}
+
+// console.log(myArray.sum());
+
+Array.prototype.average = function () {
+    return this.sum() / this.length;
+}
+
+// console.log(myArray.average());
