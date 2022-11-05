6e005219faf345d81e288fb116dab8b044f65d51
a-shiro <alekmitov20@gmail.com>
2022-11-05 12:26:31 +0200

    Added Exercise

--- JS Applications/1. Asynchronous Programming/Lab/03. Gighub-Commits/app.js
+++ JS Applications/1. Asynchronous Programming/Lab/03. Gighub-Commits/app.js
@@ -1,4 +1,32 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById('username').value;
+    const repository = document.getElementById('repo').value
+    const commitsList = document.getElementById('commits');
+
+    fetch(`https://api.github.com/repos/${username}/${repository}/commits`)
+        .then(response => {
+            if (response.ok == false) {
+                throw new Error(`Error: ${response.status} (Not Found)`);
+            }
+
+            return response.json()
+        })
+        .then(data => {
+            const commitsChildren = [];
+            console.log(data)
+            data.forEach(element => {
+                const li = document.createElement('li');
+                li.textContent = `${element.commit.author.name}: ${element.commit.message}`
+
+                commitsChildren.push(li);
+            })
+
+            commitsList.replaceChildren(...commitsChildren);
+        })
+        .catch(error => {
+            const li = document.createElement('li');
+
+            li.textContent = error.message
+            commitsList.append(li)
+        })
 }
