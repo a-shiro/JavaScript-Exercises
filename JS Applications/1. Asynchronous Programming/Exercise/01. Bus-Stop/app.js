409848ebbe486a0fd6576fe91a8a9d2c0afbfb26
a-shiro <alekmitov20@gmail.com>
2022-11-11 12:51:15 +0200

    Added Exercise

--- JS Applications/1. Asynchronous Programming/Exercise/01. Bus-Stop/app.js
+++ JS Applications/1. Asynchronous Programming/Exercise/01. Bus-Stop/app.js
@@ -1,3 +1,35 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopName = document.getElementById('stopName');
+    const busesList = document.getElementById('buses');
+    const stopId = document.getElementById('stopId');
+
+    try {
+    const stopInfo = await getStopInfo(stopId.value);
+
+    stopName.innerText = stopInfo.name;
+    busesList.innerHTML = ''
+
+    Object.entries(stopInfo.buses).forEach(([key, value]) => {
+        const li = document.createElement('li');
+        li.innerText = `Bus ${key} arrives in ${value} minutes`;
+
+        busesList.append(li);
+    })
+
+    } catch (error) {
+        stopName.innerText = 'Error';
+        busesList.innerHTML = '';
+    }
+}
+
+async function getStopInfo(id) {
+    const response = await fetch('http://localhost:3030/jsonstore/bus/businfo/' + id);
+
+    if (response.statusText == 'No Content') {
+        throw new Error();
+    }
+
+    const data = await response.json();
+
+    return data;
 }
