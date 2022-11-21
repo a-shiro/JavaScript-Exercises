980cb1d1df98f7b5f21ecdfecbaf55ddcdc62f11
a-shiro <alekmitov20@gmail.com>
2022-11-21 13:14:29 +0200

    Added Login Functionality

--- JS Applications/2. Data and Authentication/Lab/01. Cookbook - Part 2/src/app.js
+++ JS Applications/2. Data and Authentication/Lab/01. Cookbook - Part 2/src/app.js
@@ -1,48 +1,38 @@
-window.addEventListener('load', async () => {
-    const main = document.querySelector('main');
+function fillDetails(list, element,  collection) {
+    collection.forEach(item => {
+        const requestedElement = document.createElement(element);
+        requestedElement.innerText = item;
 
-    const recipes = await getRecipes()
-    const cards = recipes.map(createCard)
-
-    main.replaceChildren(...cards);
-})
-
-async function getRecipes() {
-    const response = await fetch('http://localhost:3030/jsonstore/cookbook/recipes');
-    const data = await response.json();
-
-    return Object.values(data);
-}
-
-async function getRecipeById(id) {
-    const response = await fetch('http://localhost:3030/jsonstore/cookbook/details/' + id);
-
-    return await response.json();
+        list.append(requestedElement);
+    });
 }
 
 function createCard(recipe) {
     const card = document.createElement('article');
+
     card.classList.add('preview');
     card.style.display = 'block';
-    card.addEventListener('click', toggleCard);
 
     card.innerHTML = `<div class="title">
                           <h2>${recipe.name}</h2>
                       </div>
                       <div class="small">
-                          <img src="${recipe.img}" alt="recipe-image">
-                      </div>`
+                          <img src="${recipe.img}" alt="">
+                      </div>`;
+
+    card.addEventListener('click', toggleCard);
 
     return card;
 
     async function toggleCard() {
-        const fullRecipeDetails = await getRecipeById(recipe._id);
+        const recipeDetails = await getRecipeById(recipe._id);
 
-        card.classList.remove('preview')
-        card.innerHTML = `<h2>Title</h2>
+        card.classList.remove('preview');
+
+        card.innerHTML = `<h2>${recipeDetails.name}</h2>
                           <div class="band">
                               <div class="thumb">
-                                  <img src="${fullRecipeDetails.img}">
+                                  <img src="${recipeDetails.img}" alt="">
                               </div>
                               <div class="ingredients">
                                   <h3>Ingredients:</h3>
@@ -51,24 +41,50 @@ function createCard(recipe) {
                           </div>
                           <div class="description">
                               <h3>Preparation:</h3>
-                          </div>`
+                          </div>`;
 
-        const ul = card.querySelector('ul')
+        const ingredientsList = card.querySelector('ul');
+        const preparationSteps = card.querySelector('.description');
 
-        fullRecipeDetails.ingredients.forEach(ingredient => {
-            const li = document.createElement('li');
-            li.innerText = ingredient;
-
-            ul.append(li);
-        })
-
-        const div = card.querySelector('.description');
-
-        fullRecipeDetails.steps.forEach(step => {
-            const p = document.createElement('p');
-            p.innerText = step;
-
-            div.append(p);
-        })
+        fillDetails(ingredientsList, 'li', recipeDetails.ingredients);
+        fillDetails(preparationSteps, 'p', recipeDetails.steps);
     }
 }
+
+function displayCards(cards) {
+    const main = document.querySelector('main');
+
+    main.replaceChildren(...cards);
+}
+
+async function getRecipes() {
+    const response = await fetch('http://localhost:3030/data/recipes?select=_id%2Cname%2Cimg');
+    const data = await response.json();
+
+    return Object.values(data);
+}
+
+async function getRecipeById(id) {
+    const response = await fetch('http://localhost:3030/data/recipes/' + id);
+    const data = await response.json();
+
+    return data;
+}
+
+function isUserLogged() {
+    if (sessionStorage.length === 0) {
+        return document.querySelector('#guest').style.display = 'inline-block';
+    }
+
+    return document.querySelector('#user').style.display = 'inline-block';
+}
+
+async function startApp() {
+    const recipes = await getRecipes();
+    const cards = recipes.map(createCard);
+
+    isUserLogged();
+    displayCards(cards);
+}
+
+startApp();
