85b5ca9777ac434c83bf9222d4e2fc71282f9d97
a-shiro <alekmitov20@gmail.com>
2022-11-22 13:27:00 +0200

    Added Exercise

--- JS Applications/2. Data and Authentication/Exercise/03. Students/app.js
+++ JS Applications/2. Data and Authentication/Exercise/03. Students/app.js
@@ -1,1 +1,45 @@
-console.log('TODO...');
+document.querySelector('#form').addEventListener('submit', createRecord);
+
+displayStudents();
+
+async function createRecord(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target)
+    let {firstName, lastName, facultyNumber, grade} = Object.fromEntries(formData.entries());
+
+    if (firstName && lastName && facultyNumber && grade) {
+        await fetch('http://localhost:3030/jsonstore/collections/students', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({firstName, lastName, facultyNumber, grade})
+        })
+    }
+
+    document.querySelectorAll('input').forEach(input => input.value = '')
+
+    await displayStudents();
+}
+
+async function displayStudents() {
+    const response = await fetch('http://localhost:3030/jsonstore/collections/students');
+    const students = await response.json();
+
+    const tbody = document.querySelector('tbody');
+    tbody.innerHTML = '';
+
+    for (let student of Object.values(students)) {
+        const tr = document.createElement('tr');
+
+        tr.innerHTML =
+            `<td>${student.firstName}</td>
+             <td>${student.lastName}</td>
+             <td>${student.facultyNumber}</td>
+             <td>${student.grade}</td>`
+
+        tbody.append(tr);
+    }
+}
+
