a524a5f48b8c4c4ad364dc14743fd25df9dad371
a-shiro <alekmitov20@gmail.com>
2022-11-23 10:44:32 +0200

    Added Files

---
+++ JS Applications/3. Single Page Application/Lab/02. Cookbook - Part 3/src/login.js
@@ -0,0 +1,30 @@
+document.querySelector('#login-form').addEventListener('submit', onLogin);
+
+async function onLogin(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const {email, password} = Object.fromEntries(formData.entries());
+
+    try {
+        const response = await fetch('http://localhost:3030/users/login', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            sessionStorage.setItem('accessToken', data.accessToken);
+            window.location = 'index.html';
+
+        } else {
+            throw new Error(data.message);
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+}
