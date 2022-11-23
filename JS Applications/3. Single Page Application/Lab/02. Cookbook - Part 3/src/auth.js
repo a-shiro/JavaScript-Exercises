b1968367df1a68a0d4358ce360cf7154d59f825f
a-shiro <alekmitov20@gmail.com>
2022-11-23 16:10:21 +0200

    Refactored auth.js

--- JS Applications/3. Single Page Application/Lab/02. Cookbook - Part 3/src/auth.js
+++ JS Applications/3. Single Page Application/Lab/02. Cookbook - Part 3/src/auth.js
@@ -1,18 +1,33 @@
-// Login
+export async function onRegister(event) {
+    event.preventDefault();
+
+    const {email, password, rePass} = getFormFields(event);
+
+    await sendRequest('http://localhost:3030/users/register', {email, password, rePass});
+}
 
 export async function onLogin(event) {
     event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const {email, password} = Object.fromEntries(formData.entries());
+    const {email, password} = getFormFields(event);
 
+    await sendRequest('http://localhost:3030/users/login', {email, password});
+}
+
+function getFormFields(event) {
+    const formData = new FormData(event.target);
+
+    return Object.fromEntries(formData.entries());
+}
+
+async function sendRequest(url, body) {
     try {
-        const response = await fetch('http://localhost:3030/users/login', {
+        const response = await fetch(url, {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({email, password})
+            body: JSON.stringify(body)
         });
 
         const data = await response.json();
@@ -20,7 +35,6 @@ export async function onLogin(event) {
         if (response.ok) {
             sessionStorage.setItem('accessToken', data.accessToken);
             window.location = 'index.html';
-
         } else {
             throw new Error(data.message);
         }
@@ -29,47 +43,9 @@ export async function onLogin(event) {
     }
 }
 
-// Logout
-
 export function logout() {
-    sessionStorage.clear()
-    window.location = 'index.html'
-}
-
-// Register
-
-export async function onRegister(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-
-    const {email, password, rePass} = Object.fromEntries(formData.entries());
-
-    try {
-        if (password !== rePass) {
-            throw new Error("Passwords don't match!");
-        }
-
-        const response = await fetch('http://localhost:3030/users/register', {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({email, password, rePass})
-        })
-
-        const data = await response.json();
-
-        if (response.ok) {
-            sessionStorage.setItem('accessToken', data.accessToken);
-            window.location = 'index.html';
-        } else {
-            throw new Error(data.message);
-        }
-
-    } catch (err) {
-        console.log(err.message);
-    }
+    sessionStorage.clear();
+    window.location = 'index.html';
 }
 
 export function isUserLogged() {
