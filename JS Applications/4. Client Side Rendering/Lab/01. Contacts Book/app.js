0234df87f5277ff865caa4cf759b03463f34b208
a-shiro <alekmitov20@gmail.com>
2022-12-01 12:01:11 +0200

    Refactored Code

--- JS Applications/4. Client Side Rendering/Lab/01. Contacts Book/app.js
+++ JS Applications/4. Client Side Rendering/Lab/01. Contacts Book/app.js
@@ -1,39 +1,43 @@
-import { html, nothing, render } from "../../../../node_modules/lit-html/lit-html.js";
+/** @format */
+
+import {
+  html,
+  nothing,
+  render,
+} from "../../../../node_modules/lit-html/lit-html.js";
 import { contacts as data } from "./contacts.js";
 
-const contacts = data.map(c => Object.assign({}, c, { active: false }));
+const contacts = data.map((c) => Object.assign({}, c, { active: false }));
 const root = document.getElementById("contacts");
 
-const contactCard = (contact) => html`
-<div class="contact card">
+const contactCard = (contact) => html` <div class="contact card">
   <div>
     <i class="far fa-user-circle gravatar"></i>
   </div>
   <div class="info">
     <h2>Name: ${contact.name}</h2>
     <button id=${contact.id} class="detailsBtn">Details</button>
-    
-    ${contact.active
-        ? html`<div class="details">
-         <p>Phone number: ${contact.phoneNumber}</p>
-         <p>Email: ${contact.email}</p>
-       </div>`
-        : nothing}
 
+    ${contact.active
+      ? html`<div class="details">
+          <p>Phone number: ${contact.phoneNumber}</p>
+          <p>Email: ${contact.email}</p>
+        </div>`
+      : nothing}
   </div>
 </div>`;
 
-root.addEventListener('click', toggleDetails);
+root.addEventListener("click", toggleDetails);
 
 render(contacts.map(contactCard), root);
 
 function toggleDetails(event) {
-    if (event.target.classList.contains('detailsBtn')) {
-        const id = event.target.id;
-        const contact = contacts.find(c => c.id == id);
+  if (event.target.classList.contains("detailsBtn")) {
+    const id = event.target.id;
+    const contact = contacts.find((c) => c.id == id);
 
-        contact.active = !contact.active;
+    contact.active = !contact.active;
 
-        render(contacts.map(contactCard), root);
-    }
+    render(contacts.map(contactCard), root);
+  }
 }
