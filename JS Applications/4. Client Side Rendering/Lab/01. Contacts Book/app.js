5ac413a9f47c35660538111daafbdcd81ce229f5
a-shiro <alekmitov20@gmail.com>
2022-12-01 10:46:56 +0200

    Added Exercise

--- JS Applications/4. Client Side Rendering/Lab/01. Contacts Book/app.js
+++ JS Applications/4. Client Side Rendering/Lab/01. Contacts Book/app.js
@@ -1,33 +1,27 @@
-import {html, render} from 'https://unpkg.com/lit-html?module';
-import {contacts as data} from "./contacts.js";
+import { html, nothing, render } from "../node_modules/lit-html/lit-html.js";
+import { contacts as data } from "./contacts.js";
 
-
-const contacts = data.map(c => Object.assign({}, c, {active: false}))
-
-const root = document.getElementById('contacts');
+const contacts = data.map(c => Object.assign({}, c, { active: false }));
+const root = document.getElementById("contacts");
 
 const contactCard = (contact) => html`
-  <div class="contact card">
-    <div>
-      <i class="far fa-user-circle gravatar"></i>
-    </div>
-    <div class="info">
-      <h2>Name: ${contact.name}</h2>
-      <button id=${contact.id} class="detailsBtn">Details</button>
+<div class="contact card">
+  <div>
+    <i class="far fa-user-circle gravatar"></i>
+  </div>
+  <div class="info">
+    <h2>Name: ${contact.name}</h2>
+    <button id=${contact.id} class="detailsBtn">Details</button>
+    
+    ${contact.active
+        ? html`<div class="details">
+         <p>Phone number: ${contact.phoneNumber}</p>
+         <p>Email: ${contact.email}</p>
+       </div>`
+        : nothing}
 
-      ${contact.active
-              ? html`
-                <div class="details">
-                  <p>Phone number: ${contact.phoneNumber}</p>
-                  <p>Email: ${contact.email}</p>
-                </div>`
-              : html`
-                <div class="details">
-                  <p>Phone number: ${contact.phoneNumber}</p>
-                  <p>Email: ${contact.email}</p>
-                </div>`}
-    </div>
-  </div>`
+  </div>
+</div>`;
 
 root.addEventListener('click', toggleDetails);
 
