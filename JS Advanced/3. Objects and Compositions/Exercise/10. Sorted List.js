a86cff6afd4f71246a238b05ee6e69e36ca850c9
a-shiro <alekmitov20@gmail.com>
2022-12-26 11:29:38 +0200

    Added Exercise

---
+++ JS Advanced/3. Objects and Compositions/Exercise/10. Sorted List.js
@@ -0,0 +1,58 @@
+function createSortedList() {
+    const collection = [];
+
+    return {
+        add,
+        remove,
+        get,
+        size: 0,
+    }
+
+    function add(element) {
+        collection.push(element);
+        this.size++;
+        collection.sort((a, b) => a - b);
+    };
+
+    function remove(index) {
+        const valid = checkIndex(index);
+
+        if (valid) {
+            collection.splice(index, 1);
+            this.size--;
+            
+            return;
+        }
+
+        throw new Error('Invalid index!')
+    };
+
+    function get(index) {
+        const valid = checkIndex(index);
+
+        if (valid) {
+            return collection[index];
+        }
+
+        throw new Error('Invalid index!')
+    };
+
+    function checkIndex(index) {
+        return index < collection.length && index >= 0;
+    }
+}
+
+
+let list = createSortedList();
+
+list.add(5);
+list.add(6);
+list.add(7);
+
+console.log(list.get(1));
+
+list.remove(0);
+
+console.log(list.get(1));
+
+console.log(list.size)
