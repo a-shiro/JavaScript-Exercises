14ad14e0ecbbaa596e12d5a1d07dc10699b74bef
a-shiro <alekmitov20@gmail.com>
2023-01-03 13:34:45 +0200

    Added Exercise

--- JS Advanced/5. DOM Manipulations and Events/Exercise/08. Sudomu/solution.js
+++ JS Advanced/5. DOM Manipulations and Events/Exercise/08. Sudomu/solution.js
@@ -1,4 +1,51 @@
 function solve() {
+    function checkBoard() {
+        const incorrect = '2px solid red';
+        const correct = '2px solid green'
+        row = '';
 
-    //TODO...
+        for (const field of fields) {
+            if (row.length === 3) {
+                row = '';
+            }
+
+            if (field.value === '') {
+                table.style.border = incorrect;
+                result.style.color = 'red';
+                result.textContent = 'Please fill all the fields!';
+                return;
+            }
+
+            if (Number(field.value) > 3 || Number(field.value) < 1) {
+                table.style.border = incorrect;
+                result.style.color = 'red';
+                result.textContent = 'Numbers must be between 1 and 3!';
+                return;
+            }
+
+            if (row.includes(field.value)) {
+                table.style.border = incorrect;
+                result.style.color = 'red';
+                result.textContent = 'NOP! You are not done yet...';
+                return;
+            }
+
+            row += field.value;
+        }
+
+        table.style.border = correct;
+        result.style.color = 'green';
+        result.textContent = 'You solve it! Congratulations!';
+    }
+
+    const fields = Array.from(document.querySelectorAll('input'));
+    const table = document.querySelector('table');
+    const result = document.querySelector('p');
+
+    document.querySelectorAll('button')[0].addEventListener('click', checkBoard)
+    document.querySelectorAll('button')[1].addEventListener('click', () => {
+        fields.forEach(field => field.value = '');
+        table.style.border = 'none';
+        result.textContent = ''
+    })
 }
