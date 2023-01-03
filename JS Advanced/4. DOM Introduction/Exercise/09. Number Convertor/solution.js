bfab1fd0101b26902db4d9d829085fc00c4af414
a-shiro <alekmitov20@gmail.com>
2023-01-03 12:34:42 +0200

    Added Exercise

--- JS Advanced/4. DOM Introduction/Exercise/09. Number Convertor/solution.js
+++ JS Advanced/4. DOM Introduction/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,31 @@
 function solve() {
+    document.querySelector('button').addEventListener('click', () => {
+        let decimal = document.querySelector('#input').value;
+        const convertTo = selectMenuTo.value;
 
-    //TODO...
+        switch (convertTo) {
+            case 'binary':
+                let binary = "";
+                while (decimal > 0) {
+                    if (decimal & 1) {
+                        binary = "1" + binary;
+                    } else {
+                        binary = "0" + binary;
+                    }
+                    decimal = decimal >> 1;
+                }
+
+                document.querySelector('#result').value = binary
+                break;
+            case 'hexadecimal':
+                document.querySelector('#result').value = Number(decimal).toString(16).toUpperCase()
+                break;
+        }
+    });
+
+    const selectMenuTo = document.querySelector('#selectMenuTo');
+
+    selectMenuTo.innerHTML = `
+    <option value="binary">Binary</option>
+    <option value="hexadecimal">Hexadecimal</option>`
 }
