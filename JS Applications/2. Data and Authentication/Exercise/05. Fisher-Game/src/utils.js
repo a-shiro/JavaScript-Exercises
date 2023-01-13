8008912f5dbe0fbd673e24c908cd84bb2da955a3
a-shiro <alekmitov20@gmail.com>
2023-01-13 15:08:27 +0200

    Refactored Nav Display When User Logs In/Out

--- JS Applications/2. Data and Authentication/Exercise/05. Fisher-Game/src/utils.js
+++ JS Applications/2. Data and Authentication/Exercise/05. Fisher-Game/src/utils.js
@@ -1,4 +1,8 @@
 const sections = document.querySelectorAll('section');
+const welcomeSpan = document.querySelector('nav p span');
+const loginBtn = document.querySelector('#login');
+const registerBtn = document.querySelector('#register');
+const logoutBtn = document.querySelector('#logout');
 
 export function hideSections() {
     sections.forEach(x => {
@@ -8,6 +12,18 @@ export function hideSections() {
 
 export function render(view) {
     view();
+
+    if (sessionStorage.length > 1) {
+        welcomeSpan.textContent = sessionStorage.email
+        loginBtn.style.display = 'none';
+        registerBtn.style.display = 'none';
+        logoutBtn.style.display = 'inline-block';
+    } else {
+        welcomeSpan.textContent = 'guest';
+        loginBtn.style.display = 'inline-block';
+        registerBtn.style.display = 'inline-block';
+        logoutBtn.style.display = 'none';
+    }
 }
 
 export function getFormData(e) {
